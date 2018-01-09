60e77d715ce5581fb09eb2a04bec2caacb44ccbe
Joey Biotti <joey@MacBook-Pro-2.local>
2018-01-08 20:12:42 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// @flow
+
+// VARS
+let name : string = 'John Doe';
+let age : number = 40;
+let something : any = [1,2,3];
+let users : string[] = ['John', 'Bob'];
+let hasDogs: boolean;
+
+hasDogs = true;
+
